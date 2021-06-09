fddb0bec3a54946b63c05962902f061b71c93b1c
Iony Ranarilala <ionyranarilala@gmail.com>
2021-06-09 22:21:15 +0400

    faq

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import QuestionList from './Pages/QuestionList/QuestionList'
+import Answer from './Pages/Answer/Answer'
+import Ask from './Pages/Ask/Ask'
 import './App.css';
 
-function App() {
+const initial_questions = [{
+  title: "Change column type",
+  content: "Hello all, can anyone please show how to change the type of a column in an SQL database?",
+  postDate: new Date('2021-06-07'),
+  user: "John Smith",
+  answers: [{
+    user: "Mary Simons",
+    content: "Hi, you can use the Alter statement",
+    postDate: new Date('2021-06-07')
+  }]
+}]
+
+export default function BasicExample() {
+  const [questionList, setQuestionList] = useState(initial_questions);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <QuestionList questionList={questionList} />
+          </Route>
+          <Route path="/questions">
+            <QuestionList questionList={questionList} />
+          </Route>
+          <Route path="/answer">
+            <Answer questionList={questionList} setQuestionList={setQuestionList} />
+          </Route>
+          <Route path="/ask">
+            <Ask questionList={questionList} setQuestionList={setQuestionList} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
